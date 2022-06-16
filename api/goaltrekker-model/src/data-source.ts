186161db5bd92b91919d06b6e3e1ce6022ea4601
Kristof Siket <kristofsiket@kinsta.com>
2022-06-16 16:26:03 +0200

    added ternary for the authSource for env types

--- api/goaltrekker-model/src/data-source.ts
+++ api/goaltrekker-model/src/data-source.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
     database: MONGODB_DBNAME,
     username: MONGODB_USER,
     password: MONGODB_PASSWORD,
-    authSource: "admin",
+    authSource: process.env.NODE_ENV === "development" ? "admin" : undefined,
     port: parseInt(MONGODB_PORT),
     synchronize: true,
     logging: false,
